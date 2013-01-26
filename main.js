a95cf1b76fa2e6bd4044eab93018cac0297554c0
Alex Angelini <alex.louis.angelini@gmail.com>
2013-01-26 12:16:40 -0500

    Added sample API connection

---
+++ main.js
@@ -0,0 +1,13 @@
+var app = {
+  init: function() {
+    $.getJSON('/api', function(res) {
+      if (res && res.status == 'OK') {
+        $('.container').text('CONNECTED TO THE API');
+      }
+    });
+  }
+};
+
+jQuery(function() {
+  app.init();
+});
